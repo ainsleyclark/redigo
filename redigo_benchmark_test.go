165e2710d078668e18307234c746ec52292f244b
Ainsley Clark <info@ainsleyclark.com>
2022-05-29 14:41:52 +0100

    Added graphs

--- redigo_benchmark_test.go
+++ redigo_benchmark_test.go
@@ -33,17 +33,20 @@ func BenchmarkEncode(b *testing.B) {
 	b.ReportAllocs()
 
 	for _, merge := range merges {
-		b.Run(merge.name, func(b *testing.B) {
-			m := createMap(100)
-			b.ResetTimer()
-			for i := 0; i < b.N; i++ {
-				res, err := merge.enc.Encode(m)
-				_ = res
-				if err != nil {
-					b.Logf("Error during benchmark: %s", err.Error())
+		for _, size := range sizes {
+			b.Run(merge.name, func(b *testing.B) {
+				m := createMap(100)
+				b.ResetTimer()
+				for i := 0; i < b.N; i++ {
+					res, err := merge.enc.Encode(m)
+					_ = res
+					if err != nil {
+						b.Logf("Error during benchmark: %s", err.Error())
+					}
 				}
-			}
-		})
+			})
+		}
+
 	}
 }
 
